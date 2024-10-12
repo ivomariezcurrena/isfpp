4ba1c151b65e82ced86877059cb72d5c1db34005
Capi <ivomariezcurrema@hotmail.com>
2024-10-12 19:59:43 -0300

    algunos pom para el loggin

--- src/main/java/com/example/red/negocio/Calculo.java
+++ src/main/java/com/example/red/negocio/Calculo.java
@@ -16,13 +16,13 @@ import com.example.red.modelo.Conexion;
 import com.example.red.modelo.Equipo;
 
 public class Calculo {
-    private Coordinador coordinador;
+	private Coordinador coordinador;
 	private Graph<Equipo, DefaultWeightedEdge> red; // red para hacer los cálculos
 	private Map<String, Equipo> tablaEquipos; // "ID" -> Equipo
 
-    public void setCoordinador(Coordinador coordinador){
-        this.coordinador = coordinador;
-    }
+	public void setCoordinador(Coordinador coordinador) {
+		this.coordinador = coordinador;
+	}
 
 	// Se cargan los equipos y conexiones a una red que se usará para los cálculos
 	public void cargarDatos(Map<String, Equipo> tablaEquipos, List<Conexion> conexiones) {
@@ -30,7 +30,7 @@ public class Calculo {
 		this.tablaEquipos = tablaEquipos;
 
 		// Se cargan los equipos al grafo "red"
-		for (Equipo equipo : tablaEquipos.values()) 
+		for (Equipo equipo : tablaEquipos.values())
 			red.addVertex(equipo);
 
 		// Se cargan las conexiones
@@ -44,7 +44,7 @@ public class Calculo {
 				// El peso es la menor velocidad entre la velocidad de cable y la de los puertos
 				int velocidadMaxima = Math.min(source.getVelocidadPuerto(0), target.getVelocidadPuerto(0));
 				velocidadMaxima = Math.min(velocidadMaxima, conexion.getTipocable().getVelocidad());
-				double peso = (1/ (double) velocidadMaxima);
+				double peso = (1 / (double) velocidadMaxima);
 				red.setEdgeWeight(edge, peso);
 			}
 		}
@@ -56,32 +56,36 @@ public class Calculo {
 	 * se transmiten los datos.
 	 * 
 	 * @param ID del equipo origen
+	 * 
 	 * @param ID del equipo destino
-	 * @return lista de IDs de los equipos que forman el camino, null si no existe camino
+	 * 
+	 * @return lista de IDs de los equipos que forman el camino, null si no existe
+	 * camino
 	 */
-	public List<String> traceRoute(String origen, String destino) {		
+	public List<String> traceRoute(String origen, String destino) {
 		List<String> resultado = new ArrayList<String>();
 		// Obtener los equipos
 		Equipo equipoOrigen = tablaEquipos.get(origen);
 		Equipo equipoDestino = tablaEquipos.get(destino);
-		
+
 		// Si al menos uno no se encontró se termina el método
 		if (!red.containsVertex(equipoOrigen) || !red.containsVertex(equipoDestino)) {
 			return null;
 		}
 
 		// Hallar el camino, sin importar la inactividad de los equipos
-		DijkstraShortestPath<Equipo, DefaultWeightedEdge> copia = new DijkstraShortestPath<Equipo, DefaultWeightedEdge>(red);
-		GraphPath<Equipo,DefaultWeightedEdge> camino = copia.getPath(equipoOrigen, equipoDestino);
-		if (camino == null) //no existe camino
+		DijkstraShortestPath<Equipo, DefaultWeightedEdge> copia = new DijkstraShortestPath<Equipo, DefaultWeightedEdge>(
+				red);
+		GraphPath<Equipo, DefaultWeightedEdge> camino = copia.getPath(equipoOrigen, equipoDestino);
+		if (camino == null) // no existe camino
 			return null;
 
 		// Cortar el camino ante el primer equipo inactivo
 		// Se retorna la lista de codigos de equipos
-		for (Equipo equipo : camino.getVertexList()){
+		for (Equipo equipo : camino.getVertexList()) {
 			if (equipo.isActivo())
 				resultado.add(equipo.getCodigo());
-			else 
+			else
 				break;
 		}
 		return resultado;
@@ -91,6 +95,7 @@ public class Calculo {
 	 * Realizar un ping a un equipo
 	 * 
 	 * @param ID del equipo
+	 * 
 	 * @return true si está activo, false si está inactivo
 	 */
 	public boolean ping(String destino) {
@@ -104,12 +109,13 @@ public class Calculo {
 	 * Realizar un ping a una lista de equipos
 	 * 
 	 * @param lista de "ID" de equipos
+	 * 
 	 * @return Mapa con entries "ID" -> true/false segun su actividad
 	 */
-	public Map<String, Boolean> rangoPing(List<String> ids){
+	public Map<String, Boolean> rangoPing(List<String> ids) {
 		Map<String, Boolean> resultado = new TreeMap<>();
 
-		for (String id : ids){
+		for (String id : ids) {
 			Equipo equipo = tablaEquipos.get(id);
 			resultado.put(id, equipo.isActivo());
 		}
